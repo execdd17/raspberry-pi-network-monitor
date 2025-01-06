0642442dee00e3402ba0ddfce394d1f4dfb0fc76
Alexander Vanadio <avanadio@gmail.com>
2025-01-05 23:25:44 -0500

    updated sql init to have the MACs that I know about

--- postgres/init.sql
+++ postgres/init.sql
@@ -9,6 +9,58 @@ CREATE TABLE IF NOT EXISTS devices (
     last_seen    TIMESTAMP NOT NULL
 );
 
--- If you want more tables or initial data, add it here
--- INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
--- VALUES ('AA:BB:CC:DD:EE:FF', '192.168.1.100', 'SampleVendor', false, NOW(), NOW());
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('8C:3B:AD:CF:8E:62', '192.168.1.1', 'NETGEAR', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('B0:4A:39:71:4C:DB', '192.168.1.100', 'Beijing Roborock Technology Co., Ltd.', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('D4:91:0F:9E:0F:8C', '192.168.1.102', 'Amazon Technologies Inc.', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('BC:45:5B:9E:81:AA', '192.168.1.105', 'Samsung Electronics Co.,Ltd', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('10:09:F9:97:7B:D8', '192.168.1.106', 'Amazon Technologies Inc.', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('68:DB:F5:4A:E9:7A', '192.168.1.108', 'Amazon Technologies Inc.', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('F8:54:B8:7B:91:B4', '192.168.1.109', 'Amazon Technologies Inc.', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('A8:B5:7C:79:36:80', '192.168.1.111', 'Roku, Inc', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('8C:3B:AD:CD:78:A8', '192.168.1.113', 'NETGEAR', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('AC:63:BE:B6:1A:55', '192.168.1.114', 'Amazon Technologies Inc.', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('9C:6B:00:1E:D3:AA', '192.168.1.115', 'ASRock Incorporation', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('6C:7E:67:B9:EE:F2', '192.168.1.129', 'Apple, Inc.', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('E0:F6:B5:9B:69:0B', '192.168.1.136', 'Nintendo Co.,Ltd', false, NOW(), NOW());
+
+INSERT INTO devices (mac_address, ip_address, vendor, known, first_seen, last_seen)
+VALUES
+('B8:27:EB:01:C6:C6', '192.168.1.21', 'Raspberry Pi Foundation', false, NOW(), NOW());
